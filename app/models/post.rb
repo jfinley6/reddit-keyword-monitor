0de6edf49cdc57cf9e82b87ccf93ac9dd33ee501
johnfinley <johnfinleybremerton@gmail.com>
2022-12-21 02:52:30 -0700

    settings now depend on Setting class

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,32 +1,34 @@
 class Post < ApplicationRecord
+    validates :keywords, presence: true
     
     def self.check_reddit_posts
-        if internet_connection?
 
+        if internet_connection?
+            
             response_code = 429
             while response_code != 200
-                response = HTTParty.get("https://www.reddit.com/r/#{ENV["SUBREDDIT_NAME"]}/new.json")
+                response = HTTParty.get("https://www.reddit.com/r/#{Setting.first.subreddit_name}/new.json")
                 response_code = response.code
             end
-
+            
             response_body = JSON.parse(response.body)
             posts = response_body["data"]["children"]
 
             posts.each {|post| 
-                if ENV["MATCHING_WORDS"].split(",").any? { |string| post["data"]["title"].include? string }
+                if Setting.first.keywords.split(",").any? { |string| post["data"]["title"].downcase.include? string.downcase }
                     if Post.exists?(title: post["data"]["title"])
                         next
                     else
-                        new_post= Post.create(:title => post["data"]["title"], :url => post["data"]["permalink"])
+                        new_post = Post.create!(:title => post["data"]["title"], :url => post["data"]["permalink"], :keywords => Setting.first.keywords.to_s)
                         PostMailer.post_found(new_post[:title], new_post[:url]).deliver!
-                        logger.warn "Email sent at " + Time.now.strftime("%H:%M:%S")
+                        logger.warn "Email Sent At " + Time.now.strftime("%H:%M:%S")
                     end
                 end
             }
-            logger.warn "Done checking posts at " + Time.now.strftime("%H:%M:%S")
+            logger.warn "Done Checking Posts at " + Time.now.strftime("%H:%M:%S")
             return
         else
-            logger.warn "No internet"
+            logger.warn "No Internet"
             return
         end
     end
