39b21f65b07ce9f5deabeee27c1fb4e0926e9ec4
johnfinley <johnfinleybremerton@gmail.com>
2022-12-21 02:55:05 -0700

    add routes for posts controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "posts#index"
+  get "/check_posts", to: "posts#show" 
+  get "/edit_keywords_cookies", to: "posts#edit_keywords_cookies"
+  get "/edit_subreddit_cookies", to: "posts#edit_subreddit_cookies"
+  get "/delete_all_posts", to: "posts#delete_all_posts"
+  get "/delete_all_logs", to: "posts#delete_all_logs"
+  get "/auto_check", to: "posts#auto_check_posts"
 end
