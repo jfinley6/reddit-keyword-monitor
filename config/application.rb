a3d43da705e9e97201b42b4b04b6719802eda2b7
johnfinley <johnfinleybremerton@gmail.com>
2023-01-04 12:12:17 -0700

    add generators config

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,15 @@ module RedditMonitor
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators.system_tests = nil
+    config.generators do |g|
+      g.test_framework( 
+        :rspec,
+        fixtures: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
+      )
+    end
   end
 end
