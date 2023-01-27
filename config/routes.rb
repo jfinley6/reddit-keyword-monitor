e9f80e58703262f99f2b8b7a013f5cda22bef640
johnfinley <johnfinleybremerton@gmail.com>
2023-01-27 13:12:59 -0700

    add show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
   root "posts#index"
-  get "/check_posts", to: "posts#show" 
+  get "/check_posts", to: "posts#check_posts" 
   get "/edit_keywords_cookies", to: "posts#edit_keywords_cookies"
   get "/edit_subreddit_cookies", to: "posts#edit_subreddit_cookies"
   get "/delete_all_posts", to: "posts#delete_all_posts"
   get "/delete_all_messages", to: "posts#delete_all_messages"
   get "/auto_check", to: "posts#auto_check_posts"
   get "/delete_post/:post_id", to: "posts#delete_post"
+  resources :posts
 end
