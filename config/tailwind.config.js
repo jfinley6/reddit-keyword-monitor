ca43157eb00324686435b12546a773466a989377
johnfinley <johnfinleybremerton@gmail.com>
2024-02-22 18:33:50 -0700

    Added Flowbite configuration to project for future development

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,23 +1,25 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: [
-    "./public/*.html",
-    "./app/helpers/**/*.rb",
-    "./app/javascript/**/*.js",
-    "./app/views/**/*.{erb,haml,html,slim}",
-    "./app/components/**/*.{erb,html}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
-      },
+    content: [
+        "./public/*.html",
+        "./app/helpers/**/*.rb",
+        "./app/javascript/**/*.js",
+        "./app/views/**/*.{erb,haml,html,slim}",
+        "./app/components/**/*.{erb,html}",
+        "./node_modules/flowbite/**/*.js",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
-  },
-  plugins: [
-    require("@tailwindcss/forms"),
-    require("@tailwindcss/aspect-ratio"),
-    require("@tailwindcss/typography"),
-  ],
+    plugins: [
+        require("@tailwindcss/forms"),
+        require("@tailwindcss/aspect-ratio"),
+        require("@tailwindcss/typography"),
+        require("flowbite/plugin"),
+    ],
 };
