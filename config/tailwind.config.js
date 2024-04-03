72b2f8cf8cf80c165a3e415dd0142a43021a30bf
johnfinley <johnfinleybremerton@gmail.com>
2024-04-03 16:42:09 -0600

    Update: removed unused theme

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -9,13 +9,6 @@ module.exports = {
         "./app/components/**/*.{erb,html}",
         "./node_modules/flowbite/**/*.js",
     ],
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ["Inter var", ...defaultTheme.fontFamily.sans],
-            },
-        },
-    },
     plugins: [
         require("@tailwindcss/forms"),
         require("@tailwindcss/aspect-ratio"),
